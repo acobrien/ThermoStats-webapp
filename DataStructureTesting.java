3427f7033cc48b7154f0919cc473f4eb4c896f88
Aidan O'Brien <aobrien5750@gmail.com>
2025-02-26 11:46:46 -0500

    Create DataStructureTesting.java

---
+++ DataStructureTesting.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public class DataStructureTesting {
+    //Based on slide 4 of the presentation:
+    private HashMap<String, TreeMap<String, ArrayList<String>>> hashmap = new HashMap<>();
+}
