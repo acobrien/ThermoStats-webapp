1f6361e40bf7e268195957041d383f71244e5076
Aidan O'Brien <aobrien5750@gmail.com>
2025-02-26 19:01:04 -0500

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -2,11 +2,24 @@ import java.util.*;
 
 public class Sensor {
 
-    String name;
-    private TreeMap<String, ArrayList<Double>> dataMap;
+    private String name;
+    private TreeMap<String, LinkedHashMap<String, Double>> dayMap;
 
     public Sensor(String name) {
         this.name = name;
-        this.dataMap = new TreeMap<>();
+        this.dayMap = new TreeMap<>();
     }
+
+    public void addTemperature(String day, String time, double temperature) {
+        if (!dayMap.containsKey(day)) {
+            dayMap.put(day, new LinkedHashMap<String, Double>());
+        }
+        dayMap.get(day).put(time, temperature);
+    }
+
+    //Gets the LinkedHashMap of temps for a day.
+    public LinkedHashMap<String, Double> getDay(String day) {
+        return dayMap.get(day);
+    }
+
 }
