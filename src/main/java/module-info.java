b1d0c1619b31ab3d6204b543df420d1bbf71fd2f
Aidan O'Brien <aobrien5750@gmail.com>
2025-03-13 15:41:44 -0400

    zipped problem one

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.datastructures.thermometernetwork {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-	requires javafx.graphics;
-
-
-    opens org.datastructures.thermometernetwork to javafx.fxml;
-    exports org.datastructures.thermometernetwork;
-}
