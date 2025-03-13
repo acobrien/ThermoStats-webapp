034c9d5e53af9689a7961d92668d70da4977da5a
Aidan O'Brien <aobrien5750@gmail.com>
2025-03-13 16:21:46 -0400

    webapp

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>ThermoStats Frontend</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Frontend is running!"
+    }
+  }
+}
+</script>
