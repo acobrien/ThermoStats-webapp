02f3d9e6f508b21263a6a3a0ea46ac548dafc14d
Aidan O'Brien <aobrien5750@gmail.com>
2025-03-14 19:02:46 -0400

    router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <h1>ThermoStats Frontend</h1>
-    <p>{{ message }}</p>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label>Username:</label>
+        <input type="text" v-model="username">
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" v-model="password">
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    <p v-if="statusMessage">{{ statusMessage }}</p>
   </div>
 </template>
 
