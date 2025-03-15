0ea713d5c70d9d3de5581e1d1009e3ffa556c3d8
Aidan O'Brien <aobrien5750@gmail.com>
2025-03-15 15:28:58 -0400

    working on login

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -19,10 +19,37 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      message: "Frontend is running!"
+      username: '',
+      password: '',
+      statusMessage: ''
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      try {
+        const response = await axios.post('http://localhost:8000/token', {
+          username: this.username,
+          password: this.password
+        }, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        });
+
+        // Store the token
+        localStorage.setItem('auth_token', response.data.access_token);
+
+        // Redirect to dashboard
+        this.$router.push('/dashboard');
+      }
+      catch (error) {
+        this.statusMessage = 'Login failed. Please try again.';
+      }
     }
   }
 }
